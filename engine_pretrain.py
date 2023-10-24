a3e66c764d273361617dc1e6816393ef1080086d
Neo Yin <yinzhang@berkeley.edu>
2023-10-24 00:01:25 -0700

    bug fix

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -36,12 +36,8 @@ def train_one_epoch(model: torch.nn.Module,
     if log_writer is not None:
         print('log_dir: {}'.format(log_writer.log_dir))
 
-    for iterant in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
-
-        print(iterant)
-
-        data_iter_step, (samples, _) = iterant
-
+    for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+    
         # we use a per iteration (instead of per epoch) lr scheduler
         if data_iter_step % accum_iter == 0:
             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
