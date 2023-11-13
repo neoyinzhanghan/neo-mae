461c57592a5314f94ca9384ab22754eba3b0124e
Neo Yin <yinzhang@berkeley.edu>
2023-11-13 10:45:25 -0800

    feature extraction debug utils

--- check_dim.py
+++ check_dim.py
@@ -1,22 +1,19 @@
 import h5py
 import torch
 
-def extract_tensor_and_check_dimensions(file_path):
-    with h5py.File(file_path, 'r') as file:
-        # Iterating through top-level items to find the first dataset
-        for item in file.keys():
-            if isinstance(file[item], h5py.Dataset):
-                # Extracting the tensor from the first dataset
-                tensor_data = torch.from_numpy(file[item][:])
-                break
+# Reopening the HDF5 file to check other datasets
+file_path = '/media/hdd2/one_slide_mae/23.CFNA.9 A1 H&E _154610-patch_features.h5'
 
-        # Checking the dimensions of the extracted tensor
-        tensor_dimensions = tensor_data.shape
-        return tensor_dimensions
+# Dictionary to hold tensor data from each dataset
+tensors_from_datasets = {}
 
-# Path to your HDF5 file
-file_path = "/media/hdd2/one_slide_mae/23.CFNA.9 A1 H&E _154610-patch_features.h5"
+with h5py.File(file_path, 'r') as file:
+    for item in file.keys():
+        if isinstance(file[item], h5py.Dataset):
+            # Extracting the tensor from each dataset
+            tensors_from_datasets[item] = torch.from_numpy(file[item][:])
 
-# Using the function to get the dimensions of the tensor
-dimensions = extract_tensor_and_check_dimensions(file_path)
-print(f"The dimensions of the tensor are: {dimensions}")
+# Checking the dimensions of each extracted tensor
+tensor_dimensions = {key: tensor.shape for key, tensor in tensors_from_datasets.items()}
+tensor_dimensions
+ 
